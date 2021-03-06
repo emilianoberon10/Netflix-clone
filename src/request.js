646c543fc0bd3fb2560b0bb799896e5826fc2ffb
emilianoberon10 <emilianobeorn10@gmail.com>
2021-03-05 21:26:47 -0300

    Instalando TMDB API y Firebase

---
+++ src/request.js
@@ -0,0 +1,13 @@
+const API_KEY = "AIzaSyCXP4KB6xgmamg5yfxwLFxORLrqIfSU3Nw";
+
+const requests = {
+    fetchTrending: ``,
+    fetchTrending: ``,
+    fetchTrending: ``,
+    fetchTrending: ``,
+    fetchTrending: ``,
+    fetchTrending: ``,
+    fetchTrending: ``,
+};
+
+export default requests;
